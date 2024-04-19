f646b89198574bf99418066ca787f911b901c464
Khushanshu Yadav <khushanshuyadav2@gmail.com>
2024-04-20 02:40:18 +0530

    Third Commit

--- app/src/main/java/com/khushanshu/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/khushanshu/shoppinglistapp/ShoppingList.kt
@@ -1,5 +1,7 @@
 package com.khushanshu.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -8,11 +10,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -20,6 +29,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 
@@ -49,11 +59,16 @@ fun ShoppingListApp(){
 
         LazyColumn(modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)){
+            .padding(16.dp))
+        {
             //fillMaxSize makes sure our lazy column takes entire space available PUSHING BUTTON AT TOP AUTOMATICALLY
             //padding is of lazy column not of its content as modifier modifies the composable only
 
+
+            //BELOW BLOCK REPRESENTS THE ITEMS IN LAZY LIST WHICH IS A LIST OF "ShoppingItems" NAMED AS sItems
             items(sItems) {
+
+                ShoppingListItem(item = it, onEditClick = { /*TODO*/ },{})
 
 
             }
@@ -64,28 +79,35 @@ fun ShoppingListApp(){
             AlertDialog(
                 onDismissRequest = { showDialog=false },
                 confirmButton = {
-                                Row(modifier = Modifier.padding(8.dp).fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                                Row(modifier = Modifier
+                                    .padding(8.dp)
+                                    .fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
                                     Button(onClick = {
                                         if(itemName.isNotBlank()){
                                             val newItem=ShoppingItems(id=sItems.size+1,name=itemName, quantity =itemQuantity.toInt());
                                             sItems+=newItem;
                                             showDialog=false; //As item is added close the box
-                                            itemName="";      //resetting the value as of input text to empty again
-
+                                            itemName=""; //resetting the value as of input text to empty again
+                                            itemQuantity="";
                                         }
-                                    }) {
+                                    } ) {
                                         Text(text = "Add")
 
                                     }
 
-                                    Button(onClick = {showDialog=false}) {
+                                    Button(onClick = {
+                                        itemName="";
+                                        itemQuantity="";
+                                        showDialog=false;
+                                    }
+                                    ) {
                                         Text(text = "Cancel")
 
                                     }
 
                                 }
                 },
-                title ={Text("Add Shopping Item")},
+                title ={Text("Add Shopping Item",color= Color.White)},
                 text = {
                     Column {
                         OutlinedTextField(
@@ -109,5 +131,31 @@ fun ShoppingListApp(){
             )
         }
 
+    }
+}
+
+@Composable
+//A COMPOSABLE FUNCTION WHICH REPRESENTS THE ITEM IN LIST OR LAZY LIST
+//onEditClick is of lambda function type i.e it will be executed whenever ShoppingListItem will be called as per the composable or function body
+//passed during calling time which is done as {BODY} SO WE CREATE OUR OWN COMPOSABLE LIKE onClick
+fun ShoppingListItem(item:ShoppingItems, onEditClick:()->Unit, onDeleteClick:()->Unit){
+    Row(modifier= Modifier
+        .padding(16.dp)
+        .fillMaxWidth()
+        .border(border = BorderStroke(2.dp, Color.White), shape = RoundedCornerShape(20))
+
+    ){
+        Text(text=item.name,modifier=Modifier.padding(16.dp), color = Color.White)
+        Text(text="Quantity: ${item.quantity}",modifier=Modifier.padding(16.dp), color = Color.White)
+
+        Row {
+            //ICON BUTTON CREATES A BUTTON WITH ICON
+            IconButton(onClick =onEditClick) {
+                Icon(imageVector = Icons.Default.Edit , contentDescription =null)
+            }
+            IconButton(onClick =onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete , contentDescription =null)
+            }
+        }
     }
 }
